6e6ca76fb82efc42549acf5b05b6be23e0533007
Kosodrzewinatru <sienkiew@live.com>
2019-03-29 17:03:12 +0100

    added isMoreZeros method

--- nowa/2015/java/zad4_1.java
+++ nowa/2015/java/zad4_1.java
@@ -4,6 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class zad4_1 {
+
+    // funkcja sprawdzająca czy string ma więcej niż jedynek
+    public static Boolean isMoreZeros(String line) {
+
+        int zeros = 0, ones = 0;
+
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == '0') {
+                zeros++;
+            } else {
+                ones++;
+            }
+        }
+
+        if (zeros > ones) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
 
         // wczytywanie danych z pliku
