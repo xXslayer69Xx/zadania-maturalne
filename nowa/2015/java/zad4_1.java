2da263ae4fdaec87410872aec4c5ed6a18522746
Kosodrzewinatru <sienkiew@live.com>
2019-03-29 14:20:17 +0100

    deleted class file and fixed reading from file in zad4_1

--- nowa/2015/java/zad4_1.java
+++ nowa/2015/java/zad4_1.java
@@ -1,18 +1,21 @@
-
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class zad4_1 {
-    public static void main(String[]args) throws Exception {
-        
-        //wczytywanie danych z pliku
+    public static void main(String[] args) throws Exception {
+
+        // wczytywanie danych z pliku
         FileReader file = new FileReader("../dane/liczby.txt");
         BufferedReader reader = new BufferedReader(file);
-        
         String line = reader.readLine();
 
-        while(line != null) {
-            System.out.println(line);
+        List<String> content = new ArrayList<String>();
+
+        while (line != null) {
+            content.add(line);
+            line = reader.readLine();
         }
         file.close();
     }
